1c861fd40eac409a3d1e264aeb06b05cc770dd65
daregoodness1997 <daregoodness@gmail.com>
2022-06-26 22:26:29 +0100

    sort pagination search and filter completed

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -4,7 +4,71 @@ const { StatusCodes } = require('http-status-codes');
 const { BadRequest, NotFoundError } = require('../errors');
 
 const getAllRooms = async (req, res) => {
-  const rooms = await Room.find();
+  const {
+    query: { title, hotel, numericFilters, sort, fields },
+  } = req;
+
+  const queryObject = {};
+
+  if (hotel) {
+    // const hotelId = await Hotel.find({ name: hotel });
+    queryObject.hotel = hotel;
+  }
+
+  if (title) {
+    queryObject.title = {
+      $regex: title,
+      $options: 'i',
+    };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      match => `-${operatorMap[match]}-`
+    );
+
+    const options = ['price', 'maxPeople'];
+
+    filters = filters.split(',').forEach(item => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  let result = Room.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const rooms = await result;
+
+  console.log(queryObject);
 
   res.status(StatusCodes.OK).json({ nbHits: rooms.length, rooms });
 };
