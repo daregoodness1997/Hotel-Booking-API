2c5b40e6c90c9314d449653d7de5e3aab2ceb95a
daregoodness1997 <daregoodness@gmail.com>
2022-06-27 11:44:07 +0100

    count by City

--- routes/hotels.js
+++ routes/hotels.js
@@ -4,10 +4,14 @@ const {
   createHotel,
   updateHotel,
   deleteHotel,
+  countByCity,
+  countByType,
 } = require('../controllers/hotels');
 const router = express.Router();
 
 router.route('/').get(getAllHotels).post(createHotel);
+router.route('/countByCity').get(countByCity);
+router.route('/countByType').get(countByType);
 router.route('/:id').patch(updateHotel).delete(deleteHotel);
 
 module.exports = router;
